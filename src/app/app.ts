624fd8524c1004fc0517184a826b508ab5dfcb96
Arthur Vieira <Arthurv720@gmail.com>
2025-08-15 16:21:40 -0300

    (feat) - Estilização do componente primaryButton

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Navbar } from './_components/navbar/navbar';
+import { PrimaryButton } from './_components/primary-button/primary-button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [Navbar, PrimaryButton],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
