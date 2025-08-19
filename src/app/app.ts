c4da0bfedc457b5997a265c734e92728366d3a3c
Arthur Vieira <Arthurv720@gmail.com>
2025-08-19 09:51:11 -0300

    (style) - Componente de estilo base

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { Navbar } from './_components/navbar/navbar';
 import { PrimaryButton } from './_components/primary-button/primary-button';
 import { SecondaryButton } from './_components/secondary-button/secondary-button';
 import { ItemCertificado } from './_components/item-certificado/item-certificado';
+import { BaseUi } from './_components/base-ui/base-ui';
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, PrimaryButton, SecondaryButton, ItemCertificado],
+  imports: [Navbar, PrimaryButton, SecondaryButton, ItemCertificado, BaseUi],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
