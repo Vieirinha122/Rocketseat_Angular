e1be96c052dcdeedeecc5563ba1a7fec37590cec
Arthur Vieira <Arthurv720@gmail.com>
2025-08-19 09:35:56 -0300

    (style) - Item da lista de certificados

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { Navbar } from './_components/navbar/navbar';
 import { PrimaryButton } from './_components/primary-button/primary-button';
 import { SecondaryButton } from './_components/secondary-button/secondary-button';
+import { ItemCertificado } from './_components/item-certificado/item-certificado';
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, PrimaryButton, SecondaryButton],
+  imports: [Navbar, PrimaryButton, SecondaryButton, ItemCertificado],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
